33b133e1f41e4bede4c84aab090bd44ca989607f
Nathan Tatti <tattin@shernet.sheridancollege.ca>
2024-02-21 14:16:44 -0500

    Task1b Complete: Added script to highlight player when selected.

---
+++ Assets/Week 7/Scripts/PlayerSelector.cs
@@ -0,0 +1,33 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelector : MonoBehaviour
+{
+    public Color selectedColor;
+    public Color unselectedColor;
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Selected(false);
+    }
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+    public void Selected(bool selected)
+    {
+        if (selected == true)
+        {
+            spriteRenderer.color = selectedColor;
+        }
+        else
+        {
+            spriteRenderer.color = unselectedColor;
+        }
+    }
+}
