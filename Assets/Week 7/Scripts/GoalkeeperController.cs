a91f1a45b913b6d09708333ddb2988f68504aee1
DESKTOP-F9G7CKG\Nathan Tatti <tattin@sheridancollege.ca>
2024-02-21 23:06:15 -0500

    Task4a Complete

---
+++ Assets/Week 7/Scripts/GoalkeeperController.cs
@@ -0,0 +1,31 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalkeeperController : MonoBehaviour
+{
+    public Rigidbody2D GoalkeeperRb;
+    public Transform goalCenter;
+    public float maxDistance = 3f;
+
+    void Update()
+    {
+        if (Controller.CurrentSelection != null)
+        {
+            Vector2 playerPosition = Controller.CurrentSelection.transform.position;
+            Vector2 targetPosition = GetGoalkeeperPosition(playerPosition);
+            GoalkeeperRb.MovePosition(targetPosition);
+        }
+    }
+
+    Vector2 GetGoalkeeperPosition(Vector2 playerPosition)
+    {
+        Vector2 goalLineCenter = goalCenter.position;
+        Vector2 midpoint = (playerPosition + goalLineCenter) / 2f;
+        Vector2 toMidpoint = midpoint - goalLineCenter;
+        Vector2 goalkeeperPosition = goalLineCenter + Vector2.ClampMagnitude(toMidpoint, maxDistance);
+        return goalkeeperPosition;
+    }
+}
